467de0386644255b8062fe964f22d4816f29139a
aspd199 <artist_power@163.com>
2019-05-07 05:21:50 +0800

    Tompin19

--- test/js/main-pixi.js
+++ test/js/main-pixi.js
@@ -1,4 +1,4 @@
-var app = new PIXI.Application(1060, 760, {forceFXAA: true,antialias:true,backgroundColor: colorHex(255,250,240)});//0x1099bb
+var app = new PIXI.Application(100%, 100%, {forceFXAA: true,antialias:true,backgroundColor: colorHex(255,250,240)});//0x1099bb
 PIXI.settings.ROUND_PIXELS = true; PIXI.settings.RESOLUTION = 1;
 app.view.innerHTML = "你也不至于用如此旧的浏览器吧？";
 app.view.style = "position: fixed;left:0px; top:0px;cursor:default;"
