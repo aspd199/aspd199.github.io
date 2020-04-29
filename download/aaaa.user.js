e909e4b9636a718b2dd5d038edc53c76c8f1c04c
aspd199 <artist_power@163.com>
2020-04-30 05:09:06 +0800

    test js

---
+++ download/aaaa.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name          aaaa
+// @namespace     http://lillje.tld
+// @include       https://www.steamgifts.com/discussion/*
+// @author        lillje
+// @version       1.16
+// @downloadURL   https://gist.github.com/lillje/f836ebd0cfb861c3fab45360c5102936/raw/LilljeBarterBundleHelper.user.js
+// @updateURL     https://gist.github.com/lillje/f836ebd0cfb861c3fab45360c5102936/raw/LilljeBarterBundleHelper.user.js
+// @grant         none
+// @run-at        document-idle
+// ==/UserScript==
